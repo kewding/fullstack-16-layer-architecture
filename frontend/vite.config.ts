71984b0e1f4fbe3330d836580c5bee066012cfaf
nyric <nyric.me@gmail.com>
2026-01-27 12:19:27 +0800

    config: added svg handling

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,12 +9,14 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths' 
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),          // React support
     tsconfigPaths(),  // Respect tsconfig paths 
+    svgr(),           // SVG handling
   ],
   test: {
     globals: true,           // Allows using describe/it/test without importing
