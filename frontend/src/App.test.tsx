465ae1f6d380538514c3683d11b351d89b6744d8
nyric <nyric.me@gmail.com>
2026-01-27 12:12:26 +0800

    config: added Vitest and React testing library

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+test('renders Vite + React text', () => {
+  render(<App />)
+  expect(screen.getByText(/vite \+ react/i)).toBeInTheDocument()
+})
