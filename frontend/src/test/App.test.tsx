86617f8a00ed5ccde90242ff9ca4de4aa9e1f7aa
nyric <nyric.me@gmail.com>
2026-01-29 04:18:46 +0800

    fix: added routing within App.test.tsx to properly find the pages

--- frontend/src/test/App.test.tsx
+++ frontend/src/test/App.test.tsx
@@ -1,7 +1,21 @@
-import { render, screen } from '@testing-library/react'
-import { App } from '../app/App'
+import { render, screen } from "@testing-library/react"
+import { createMemoryRouter, RouterProvider } from "react-router-dom"
+import { adminRoutes } from "@/features/admin/routes"
+// import { authRoutes } from "@/features/auth/routes"
+// import { userRoutes } from "@/features/user/routes"
 
-test('renders Vite + React text', () => {
-  render(<App />)
+test("renders Vite + React text", () => {
+  const router = createMemoryRouter(
+    [
+      { path: "/", element: <div>Vite + React</div> }, // placeholder element for test
+      ...adminRoutes
+      // ...authRoutes,
+      // ...userRoutes
+    ],
+    { initialEntries: ["/"] } // start test at "/"
+  )
+
+  render(<RouterProvider router={router} />)
+
   expect(screen.getByText(/vite \+ react/i)).toBeInTheDocument()
 })
