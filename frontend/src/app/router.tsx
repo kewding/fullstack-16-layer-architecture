0a67578badc60b9b0c89de82380e4d9224598d5c
nyric <nyric.me@gmail.com>
2026-01-29 16:36:35 +0800

    chore: uncommented router reference

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -1,10 +1,9 @@
-// app/router.tsx
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-// import { authRoutes } from "@/features/auth/routes"
-// import { userRoutes } from "@/features/user/routes"
+import { authRoutes } from "@/features/auth/routes"
+import { userRoutes } from "@/features/user/routes"
 import { adminRoutes } from "@/features/admin/routes"
 
-const allRoutes = [/*...authRoutes, ...userRoutes,*/ ...adminRoutes]
+const allRoutes = [...authRoutes, ...userRoutes, ...adminRoutes]
 
 const router = createBrowserRouter(allRoutes)
 
