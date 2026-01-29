9fd62289dbfbd1d09284510f1ecbc06c81c18284
nyric <nyric.me@gmail.com>
2026-01-29 13:41:14 +0800

    feature: added routing for LoginPage.tsx

---
+++ frontend/src/features/auth/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const LoginPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Login Page</h1>
+        </div>
+    )
+}
---
+++ frontend/src/features/auth/routes.tsx
@@ -0,0 +1,6 @@
+import { LoginPage } from "./pages/LoginPage";
+
+export const authRoutes = [
+    {path: "/auth/login", element: <LoginPage />},
+]
+    
